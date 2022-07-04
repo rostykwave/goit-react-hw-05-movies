fed32432eb4af31b0e67a49e61b2cbf50eedcae6
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-04 18:11:00 +0300

    goit-react-hw-05-movies

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="/goit-react-hw-05-movies" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
