504fa96d8a68e40dba019f20add46cd7df8313de
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-14 13:04:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="./" element={<SharedLayout />}>
+        <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="./movies" element={<Movies />} />
-          <Route path="./movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          {/* <Route path="/goit-react-hw-05-movies" element={<Home />} /> */}
-          <Route path="./*" element={<NotFound />} />
+          <Route path="/goit-react-hw-05-movies" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
