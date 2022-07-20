7223d8828811b6a79c0e706d18cc13cdcf85e6a4
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-20 14:11:18 +0300

    routing fix

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { theme } from 'styleConfig/theme';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HashRouter>
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
         <App />
-      </HashRouter>
-      {/* <BrowserRouter>
-        <App />
-      </BrowserRouter> */}
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
